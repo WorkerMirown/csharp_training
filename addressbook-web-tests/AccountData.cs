b710e663e81dc85f3d73e2fcdf6027161822a6ee
Mirown <workerismirown@gmail.com>
2025-03-16 18:07:06 +0300

    1.2 with restuctured code

---
+++ addressbook-web-tests/AccountData.cs
@@ -0,0 +1,27 @@
+﻿namespace addressbook_web_tests.GroupTests.GroupTests
+{
+    internal class AccountData
+    {
+        private string username;
+        private string password;
+
+        public AccountData(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+
+        }
+    }
+}
